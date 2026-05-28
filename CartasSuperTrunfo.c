b2a4066f553c942d0bb1d976b1832595486cf755
Rafael9900 <160249588+Rafael9900@users.noreply.github.com>
2026-05-28 21:22:39 +0000

    Super Trunfo Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+
 int main(){
+
+    printf("**************SUPER TRUNFO***********\n");
     char estado1[1];
     char estado2[1];
     char codigo1[2];
     char codigo2[2];
     char nome1[50];
     char nome2[50];
-    int populacao1, populacao2, turistico1, turistico2;
+    unsigned long int populacao1, populacao2; 
+    int turistico1, turistico2;
     float area1, area2;
     float pib1, pib2;
 //carta 1
+
     printf("Digite a letra do nome do primeiro estado: \n");
     scanf("%s", &estado1);
 
@@ -36,6 +41,8 @@ int main(){
     float DenPop1 = (float)(populacao1 / area1);
     float PIBCap1 = (float)(pib1 / populacao1);
 
+     
+
     //Calculo carta1
     printf("Estado: '%s'",estado1);
     printf("\nCodigo: '%s'",estado1);
@@ -47,9 +54,7 @@ int main(){
     printf("\nPontos turisticos: '%d'",turistico1);
     printf("\nDensidade Populacional: '%.2f'",DenPop1);
     printf("\nPIB per Capita: '%.2f'",PIBCap1);
-
    
-
 
 
     //carta 2
@@ -77,6 +82,7 @@ int main(){
     //Densidade populacional, PIB per capita, carta2:
     float DenPop2 = (float)(populacao2 / area2);
     float PIBCap2 = (float)(pib2 / populacao2);
+    
 
     //Calculo carta2
 
@@ -90,6 +96,40 @@ int main(){
     printf("\nPontos turisticos: '%d'",turistico2);
     printf("\nDensidade Populacional: '%.2f'",DenPop2);
     printf("\nPIB per Capita: '%.2f'",PIBCap2);
+
+     //super poder carta1
+    float poder1= (float) populacao1 + area1 + pib1 + turistico1 + PIBCap1 + DenPop1;
+
+     //super poder carta2
+    float poder2= (float) populacao2 + area2 + pib2 + turistico2 + PIBCap2 + DenPop2;
+
+    //comparação cartas
+
+    printf("\n********COMPARACAO DAS CARTAS****************\n");
+
+    unsigned long int populacoes=populacao1>populacao2;
+    printf("Populacao de %s e maior que %s? %d\n ",nome1, nome2);
+    printf("%d\n",populacoes);
+
+    float areas=(float)area1>area2;
+    printf("A Area de %s e maior que %s? %d\n", nome1, nome2);
+    printf("%d\n",areas);
+
+    float PIBs=pib1>pib2;
+    printf("O PIB de %s e maior que %s? %d\n", nome1, nome2);
+    printf("%d\n");
+
+    int turisticos=turistico1>turistico2;
+    printf("Existem mais pontos turisticos em %s do que %s? %d\n", nome1, nome2);
+    printf("%d\n",turisticos);
+
+    float DenPopulacional=(float) DenPop1 > DenPop2;
+    printf("%s tem a populacao menor do que %s? %d\n", nome1, nome2);
+    printf("%d\n",DenPopulacional);
+
+    float poderes=(float)poder1>poder2;
+    printf("A cidade %s e mais poderosa que a cidade %s? %d\n", nome1, nome2);
+    printf("%d\n",poderes);
 
 
     
